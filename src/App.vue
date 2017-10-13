f0e9e5e74438f68cc2c16b199fb78d729f960016
Riyaz Ali <riyazkagzi17@gmail.com>
2017-10-13 21:39:04 +0530

    Added some bio and GitubProject cards

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,33 @@
         <topbar />
 
         <!-- Content Area -->
-        <content-holder>
+        <content-holder class="content">
           <h2 style="text-align: center; margin: 30px 0 20px;">Hello, my name is Riyaz. I'm a Fullstack Developer from India</h2>
 
           <!-- google map here -->
           <google-map />
 
+          <!-- some bio -->
+          <p class="bio">
+            I am a <span class="highlight">maker</span> and <span class="highlight">student</span> based in India. <br /><br />
+            I code in HTML/CSS/JavaScript for the <span class="highlight">web</span>, Python/Node.js for <span class="highlight">backends</span> and Java for <span class="highlight">mobiles</span>. <br /><br />
+            I also worked as an <span class="highlight">intern at <a target="_blank" href="https://my.homecampus.com.sg">HomeCampus</a></span><br /><br />
+            I am also one of the amazing mainteners of <span class="highlight"><a href="https://github.com/esfiddle/esfiddle" target="_blank">ESFiddle</a></span>
+          </p>
+
+          <!-- highlight some projects -->
+          <div class="projects">
+            <h2>Here are some of my open source projects </h2>
+            <div>
+              <github-project v-for="gh in github" :key="gh.name" :gh="gh" />
+            </div>
+          </div>
+
         </content-holder>
     </div>
 
     <!-- Footer -->
-    <f-footer style="height: 30px; line-height: 30px;"/>
+    <!--<f-footer style="height: 30px; line-height: 30px;"/>-->
   </div>
 </template>
 
@@ -24,6 +40,7 @@
 import Topbar from "./components/Topbar.vue";
 import ContentHolder from "./components/ContentHolder.vue";
 import GoogleMap from "./components/GoogleMap.vue";
+import GithubProject from "./components/GithubProject.vue";
 import FFooter from "./components/Footer.vue";
 
 // utils
@@ -34,11 +51,39 @@ export default {
     Topbar,
     ContentHolder,
     GoogleMap,
+    GithubProject,
     FFooter
   },
   data: () => ({
-    lipsum
-  })
+    lipsum,
+    github: [
+
+    ]
+  }),
+
+  mounted(){
+    let user = "riyaz-ali";
+
+    let mapRepoToObject = repo => ({
+      owner: repo.owner.login,
+      avatar: repo.owner.avatar_url + '&s=48',
+      name: repo.name,
+      language: repo.language,
+      description: repo.description,
+      forks: repo.forks_count,
+      stars: repo.stargazers_count,
+
+    });
+
+    // get repo data from github
+    fetch(`https://api.github.com/users/${user}/repos?type=all&sort=pushed`)
+      .then(response => response.json())
+      .then(repos => repos.slice(0, 5))
+      .then(repos => repos.map(mapRepoToObject))
+      .then(repoObjects => {
+        this.github = repoObjects;
+      })
+  }
 }
 </script>
 
@@ -57,12 +102,51 @@ body { height: 100%; font-family: "Karla", sans-serif; }
 }
 
 .holder {
-  height: calc(100% - 30px);
+  height: 100%;//calc(100% - 30px);
   border-radius: 1px;
   box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
 
   > .whole.column {
     height: 100%;
+  }
+}
+
+.content {
+  .bio {
+    font-size: 1.5em;
+    text-align: center;
+
+    &, & > .highlight { transition: 0.2s; }
+
+    .highlight {
+      a {
+        color: inherit;
+        text-decoration: underline;
+        text-decoration-style: dotted;
+      }
+    }
+    &:hover {
+      color: rgba(0,0,0,0.65);
+      & > .highlight {
+        color: #001F3F;
+      }
+    }
+  }
+
+  .projects {
+    margin: 80px 0 0;
+
+    h2 {
+      text-align: center;
+      border-bottom: 1px solid rgba(0,0,0,0.2);
+      padding-bottom: 15px;
+    }
+
+    > div {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+    }
   }
 }
 </style>
---
+++ src/components/GithubProject.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="project">
+    <div class="header">
+      <a class="avatar" :href="`https://github.com/${gh.owner}`" target="_blank">
+        <img :src="gh.avatar">
+      </a>
+      <strong class="name"><a href="https://github.com/riyaz-ali/portfolio" target="_blank">{{gh.name}}</a><sup class="language">{{ gh.language }}</sup></strong>
+      <span>Created by <a :href="`https://github.com/${gh.owner}`" target="_blank">{{ gh.owner }}</a></span>
+      <a class="star" title="Star the project on Github" :href="`https://github.com/${gh.owner}/${gh.name}`" target="_blank"><i class="fa fa-star"></i></a>
+    </div>
+
+    <div class="description">
+      <p>{{ gh.description }}</p>
+    </div>
+
+    <div class="footer">
+      <span class="status" :title="`${gh.forks} forks`"><i class="fa fa-code-fork"></i> {{gh.forks}}</span>
+      <span class="status" :title="`${gh.stars} stars`"><i class="fa fa-star"></i> {{gh.stars}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    gh: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.project {
+  width: 300px;
+  height: 160px;
+  margin: 15px 0;
+  //height: 80px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
+  border: 1px solid #ddd;
+  position: relative;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .header {
+    padding: 10px;
+    padding-left: 65px;
+    position: relative;
+
+    .avatar {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      height: 48px;
+      width: 48px;
+    }
+
+    .name {
+      display: block;
+      color: rgba(0,0,0,0.8);
+
+      .language {
+        color: rgba(0,0,0,0.5);
+        margin: 0 5px;
+      }
+    }
+
+    .star {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+  }
+
+  .description {
+    padding: 5px 15px;
+    overflow: hidden;
+  }
+
+  .footer {
+    position: absolute;
+    bottom:  0;
+    width: 100%;
+    padding: 5px 15px;
+    background: rgba(0,0,0,0.1);
+
+    .status {
+      margin: 0 5px;
+
+      &:first-child { margin-left: 0; }
+      &:last-child { margin-right: 0; }
+    }
+  }
+}
+</style>
