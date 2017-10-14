d7097708c1dcc29c7c8a3867da5664b81f680bb4
Riyaz Ali <riyazkagzi17@gmail.com>
2017-10-14 12:43:51 +0530

    Added social links

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
           <p class="bio">
             I am a <span class="highlight">maker</span> and <span class="highlight">student</span> based in India. <br /><br />
             I code in HTML/CSS/JavaScript for the <span class="highlight">web</span>, Python/Node.js for <span class="highlight">backends</span> and Java for <span class="highlight">mobiles</span>. <br /><br />
-            I also worked as an <span class="highlight">intern at <a target="_blank" href="https://my.homecampus.com.sg">HomeCampus</a></span><br /><br />
+            I worked as an <span class="highlight">intern at <a target="_blank" href="https://my.homecampus.com.sg">HomeCampus</a></span><br /><br />
             I am also one of the amazing mainteners of <span class="highlight"><a href="https://github.com/esfiddle/esfiddle" target="_blank">ESFiddle</a></span>
           </p>
 
@@ -24,6 +24,26 @@
             <h2>Here are some of my open source projects </h2>
             <div>
               <github-project v-for="gh in github" :key="gh.name" :gh="gh" />
+            </div>
+          </div>
+
+          <!-- network profiles -->
+          <div class="social">
+            <h2>Social</h2>
+            <p>
+              You can always find my writings on my <a href="javascript:swal('Under Construction!', 'Hey there! Thanks for your interest but my blog is still under construction! I\'ll update the link soon. Sorry for the random surprise ;)', 'warning')">Blog</a>, my thoughts on <a href="https://twitter.com/riyaz2302" target="_blank">Twitter</a> and my code on <a href="https://github.com/riyaz-ali" target="_blank">Github</a>
+            </p>
+
+            <h3>&dash; Or you can find me on &dash;</h3>
+            <div class="links">
+              <div>
+                <a class="stackoverflow" href="https://stackoverflow.com/users/6611700/riyaz-ali" target="_blank"><i class="fa fa-stack-overflow"></i> Stackoverflow</a>
+                <a class="twitter" href="https://twitter.com/riyaz2302" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
+              </div>
+              <div>
+                <a class="github" href="https://github.com/riyaz-ali" target="_blank"><i class="fa fa-github"></i> Github</a>
+                <a class="email" href="mailto:hey@iamriyaz.com?subject=Hey" target="_blank"><i class="fa fa-envelope"></i> Email</a>
+              </div>
             </div>
           </div>
 
@@ -146,6 +166,86 @@ body { height: 100%; font-family: "Karla", sans-serif; }
       display: flex;
       flex-wrap: wrap;
       justify-content: space-evenly;
+    }
+  }
+
+  .social {
+    margin: 80px 0 0;
+
+    h2 {
+      text-align: center;
+      border-bottom: 1px solid rgba(0,0,0,0.2);
+      padding-bottom: 15px;
+    }
+
+    h3 {
+      text-align: center;
+      margin: 35px 0 20px;
+    }
+
+    p {
+      text-align: center;
+      font-size: 1.3em;
+      a {
+        color: inherit;
+      }
+    }
+
+    .links {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0 auto;
+
+      @media screen and (min-width: 500px) {
+        width: 50%;
+      }
+
+      > div {
+        flex: 1;
+        //padding: 15px;
+        //border: 1px solid red;
+      }
+
+      a {
+        display: block;
+        width: 220px;
+        height: 68px;
+        margin: 15px auto 0;
+        text-align: center;
+        padding: 25px 25px 25px 50px;
+        justify-content: space-around;
+        color: #fff;
+        font-weight: bold;
+        font-size: 1em;
+        letter-spacing: 2px;
+        text-decoration: none;
+        text-transform: uppercase;
+        border-radius: 2px;
+        box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+        position: relative;
+        transition: box-shadow 0.2s;
+
+        i {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 68px;
+          line-height: 68px;
+          width: 40px;
+          background: rgba(0,0,0,0.6);
+          text-align: center;
+          font-size: 1.2em;
+        }
+
+        &:hover {
+          box-shadow: 2px 2px 3px 1px rgba(0, 0, 0, 0.6);
+        }
+
+        &.stackoverflow { background-color: #f48024; }
+        &.twitter { background-color: #1da1f2; }
+        &.github { background-color: #24292e; }
+        &.email { background-color: #ea4335; }
+      }
     }
   }
 }
